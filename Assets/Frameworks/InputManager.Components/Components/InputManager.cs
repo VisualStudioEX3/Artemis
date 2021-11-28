96ad2be81ffd61abbde0fb81419055242e4fd210
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-11-28 06:05:34 +0100

    Updated InputManager component
    - Implemented InputMapAsset system.
    - Implemented abstract MonoBehaviourSingleton<T>.
    - Added the test input map asset "Default".
    - Updated tests scene.

--- Assets/Frameworks/InputManager.Components/Components/InputManager.cs
+++ Assets/Frameworks/InputManager.Components/Components/InputManager.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Linq;
 using UnityEngine;
+using VisualStudioEX3.Artemis.Framework.Core.Contracts;
+using VisualStudioEX3.Artemis.Framework.InputManager.Contracts.Exceptions;
 using VisualStudioEX3.Artemis.Framework.InputManager.Contracts.Interfaces;
-using VisualStudioEX3.Artemis.Framework.InputManager.Contracts.Models;
+using VisualStudioEX3.Artemis.Framework.InputManager.Contracts.Models.Assets;
 using VisualStudioEX3.Artemis.Framework.InputManager.ServiceProvider;
 
 namespace VisualStudioEX3.Artemis.Framework.InputManager.Components
@@ -8,9 +12,13 @@ namespace VisualStudioEX3.Artemis.Framework.InputManager.Components
     /// <summary>
     /// Input Manager component.
     /// </summary>
-    /// <remarks>This component manages all game input states.</remarks>
-    public class InputManager : MonoBehaviour
+    /// <remarks>This component manages all game input states. It's accessible as singleton instance from others scripts.</remarks>
+    public class InputManager : MonoBehaviourSingleton<InputManager>
     {
+        #region Constants
+        private static readonly string ARGUMENT_NULL_EXCEPTION_MESSAGE = $"{nameof(InputManager)}::{{0}}: The name can not be null or empty.";
+        #endregion
+
         #region Internal vars
         private IInputAxisProcessor _inputAxisProcessor;
         private IInputActionProcessor _inputActionProcessor;
@@ -18,26 +26,69 @@ namespace VisualStudioEX3.Artemis.Framework.InputManager.Components
 
         #region Serializable fields
         [SerializeField]
-        private InputAxis[] _axes;
-        [SerializeField]
-        private InputAction[] _actions;
+        private InputMapAsset[] _inputMaps;
         #endregion
 
-        #region Initializers
-        private void Awake()
+        #region Initializers & Finalizers
+        public override void Awake()
         {
+            base.Awake();
+
             InputManagerServiceFactory serviceFactory = InputManagerServiceFactory.Factory;
 
             this._inputAxisProcessor = serviceFactory.GetService<IInputAxisProcessor>();
             this._inputActionProcessor = serviceFactory.GetService<IInputActionProcessor>();
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
         }
         #endregion
 
         #region Update logic
         private void Update()
         {
-            this._inputAxisProcessor.Update(this._axes);
-            this._inputActionProcessor.Update(this._actions);
+            foreach (InputMapAsset asset in this._inputMaps)
+                if (asset.Enabled)
+                    this.UpdateInputMapAsset(asset);
+        }
+
+        private void UpdateInputMapAsset(InputMapAsset asset)
+        {
+            this._inputAxisProcessor.Update(asset.Axes);
+            this._inputActionProcessor.Update(asset.Actions);
+        }
+        #endregion
+
+        #region Methods & Functions
+        public override bool IsPersistentBetweenScenes() => true;
+
+        /// <summary>
+        /// Gets the requested <see cref="InputMapAsset"/> instance.
+        /// </summary>
+        /// <param name="name"><see cref="InputMapAsset"/> name. Is the asset name in project.</param>
+        /// <returns>Returns the <see cref="InputMapAsset"/> instance.</returns>
+        /// <exception cref="InputMapAssetNotFoundException">If the name not matches with any element in the list then raises an exception.</exception>
+        /// <exception cref="ArgumentNullException">If the name is empty or <see langword="null"/> string.</exception>
+        public InputMapAsset GetInputMap(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw this.FormatArgumentNullException(nameof(GetInputMap));
+
+            try
+            {
+                return this._inputMaps.Where(e => e.name == name).First();
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InputMapAssetNotFoundException(name);
+            }
+        }
+
+        private ArgumentNullException FormatArgumentNullException(string name)
+        {
+            return new ArgumentNullException(string.Format(InputManager.ARGUMENT_NULL_EXCEPTION_MESSAGE, name));
         }
         #endregion
     }
