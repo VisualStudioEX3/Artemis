a8857497c7edd722adee1029de649f89ed6ca83d
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-11-28 06:03:05 +0100

    Implemented InputMapAsset scriptable object
    - Implemented in InputManager to able managing multiple maps in runtime.
    - Updated InputManager.Contracts exceptions.

---
+++ Assets/Frameworks/InputManager.Contracts/Exceptions/InputActionNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using VisualStudioEX3.Artemis.Framework.InputManager.Contracts.Models;
+
+namespace VisualStudioEX3.Artemis.Framework.InputManager.Contracts.Exceptions
+{
+    /// <summary>
+    /// Exception for when trying to access an <see cref="InputAction"/> element thats not exists.
+    /// </summary>
+    public class InputActionNotFoundException : Exception
+    {
+        #region Constructor
+        public InputActionNotFoundException(string actionName, string inputMapName)
+            : base($"The {nameof(InputAction)} \"{actionName}\" not exists in the asset \"{inputMapName}\".")
+        {
+        } 
+        #endregion
+    }
+}
