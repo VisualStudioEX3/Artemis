1d693763e08677d5569ca8809be5d848fb2276e0
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-11-29 16:53:59 +0100

    Implemented generic IUnityObjectPool service on Core.Contracts

---
+++ Assets/Frameworks/Core.Contracts/Interfaces/IUnityObjectPoolService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisualStudioEX3.Artemis.Framework.Core.Contracts.Interfaces
+{
+    /// <summary>
+    /// Generic object pool service for work with <see cref="UnityEngine.Object"/> instances.
+    /// </summary>
+    /// <typeparam name="T"><see cref="UnityEngine.Object"/> based type of the prefab used to create instances.</typeparam>
+    public interface IUnityObjectPoolService<T> : IDisposable where T : UnityEngine.Object
+    {
+        #region Properties
+        /// <summary>
+        /// Is the object pool initialized?
+        /// </summary>
+        bool IsInitialized { get; }
+        
+        /// <summary>
+        /// Read only list of all instances in this object pool.
+        /// </summary>
+        IReadOnlyList<T> Instances { get; }
+        
+        /// <summary>
+        /// Prefab model to create the instances.
+        /// </summary>
+        T Prefab { get; }
+        
+        /// <summary>
+        /// Total count of the instances in this object pool.
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Count of all available instances in this object pool.
+        /// </summary>
+        /// <see cref="AvailableInstancePredicate"/>
+        int AvailableCount { get; }
+
+        /// <summary>
+        /// Predicate to define the condition filter to get the available instances.
+        /// </summary>
+        /// <remarks>Remember to implement mechanims in the object to create this predicate.</remarks>
+        Func<T, bool> AvailableInstancePredicate { get; }
+
+        /// <summary>
+        /// Predicate to define how to release all unavailable instances.
+        /// </summary>
+        /// <remarks>Remember to implement mechanims in the object to create this predicate.</remarks>
+        Action<T> ReleaseInstancePredicate { get; } 
+        #endregion
+
+        #region Events
+        /// <summary>
+        /// Notifies when try to get an available instance but not are one available.
+        /// </summary>
+        event Action OnNotAvailableInstance;
+        #endregion
+
+        #region Methods & Functions
+        /// <summary>
+        /// Initialize this object pool.
+        /// </summary>
+        /// <param name="prefab">Prefab model to create the instances. Must be an <see cref="UnityEngine.Object"/> based type.</param>
+        /// <param name="capacity">Ammount of instances to create.</param>
+        /// <param name="availableInstancePredicate">Predicate to define the condition filter to get the available instances.</param>
+        /// <param name="releaseInstancePredicate">Predicate to define how to release all unavailable instances.</param>
+        void Initialize(T prefab, int capacity, Func<T, bool> availableInstancePredicate, Action<T> releaseInstancePredicate);
+
+        /// <summary>
+        /// Try to get the next available instance.
+        /// </summary>
+        /// <param name="instance">The available instance.</param>
+        /// <returns>Returns <see langword="true"/> if it found an available instance. In case that not have any available instance, returns <see langword="false"/> and raises the <see cref="OnNotAvailableInstance"/> event.</returns>
+        bool TryGetNext(out T instance);
+
+        /// <summary>
+        /// Release all instance.
+        /// </summary>
+        /// <see cref="ReleaseInstancePredicate"/>
+        void ReleaseAllInstances(); 
+        #endregion
+    }
+}
---
+++ Assets/Frameworks/Core.Services/Services/UnityObjectPoolService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using VisualStudioEX3.Artemis.Framework.Core.Contracts.Interfaces;
+
+namespace VisualStudioEX3.Artemis.Framework.Core.Services
+{
+    public class UnityObjectPoolService<T> : IUnityObjectPoolService<T> where T : UnityEngine.Object
+    {
+        #region Internal vars
+        private bool _disposedValue;
+        private List<T> _instances;
+        #endregion
+
+        #region Properties
+        public bool IsInitialized => this._instances is not null;
+        public IReadOnlyList<T> Instances => this._instances;
+        public int Count => this._instances.Count;
+        public int AvailableCount => this._instances.Count(this.AvailableInstancePredicate);
+        public T Prefab { get; private set; }
+        public Func<T, bool> AvailableInstancePredicate { get; private set; }
+        public Action<T> ReleaseInstancePredicate { get; private set; }
+        #endregion
+
+        #region Events
+        public event Action OnNotAvailableInstance;
+        #endregion
+
+        #region Destructor
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposedValue)
+            {
+                if (disposing)
+                    this.DestroyAllInstances();
+
+                this._disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        #region Methods & Functions
+        public void Initialize(T prefab, int capacity, Func<T, bool> availableInstancePredicate, Action<T> releaseInstancePredicate)
+        {
+            this._instances = new List<T>(capacity);
+
+            this.Prefab = prefab;
+            this.AvailableInstancePredicate = availableInstancePredicate;
+            this.ReleaseInstancePredicate = releaseInstancePredicate;
+
+            this.CreateAllInstances();
+        }
+
+        private void CreateAllInstances()
+        {
+            for (int i = 0; i < this.Count; i++)
+                this._instances.Add(this.CreateInstance());
+        }
+
+        private T CreateInstance() => GameObject.Instantiate<T>(this.Prefab);
+
+        private void DestroyAllInstances()
+        {
+            for (int i = 0; i < this.Count; i++)
+                this.DestroyInstance(i);
+        }
+
+        private void DestroyInstance(int index) => GameObject.DestroyImmediate(this._instances[index]);
+
+        public bool TryGetNext(out T instance)
+        {
+            try
+            {
+                instance = this._instances.First(this.AvailableInstancePredicate);
+
+                return true;
+            }
+            catch
+            {
+                this.OnNotAvailableInstance?.Invoke();
+                instance = null;
+
+                return false;
+            }
+        }
+
+        public void ReleaseAllInstances()
+        {
+            foreach (T instance in this._instances)
+                this.ReleaseInstancePredicate(instance);
+        }
+        #endregion
+    }
+}
