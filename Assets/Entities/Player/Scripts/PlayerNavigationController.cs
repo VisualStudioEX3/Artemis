c8cbac04fb007bc43bd5f2f6f8a090bbef416ed8
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-11-29 01:31:20 +0100

    Fixed mixed navigation behaviour
    - Implemented separated actions to enable mouse and keyboard navigation.
    - Implemneted separated axis to navigate with mouse and keyboard.
    - Refactorized PlayerNavigationController.

--- Assets/Entities/Player/Scripts/PlayerNavigationController.cs
+++ Assets/Entities/Player/Scripts/PlayerNavigationController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using VisualStudioEX3.Artemis.Framework.InputManager.Components;
 using VisualStudioEX3.Artemis.Framework.InputManager.Contracts.Models;
@@ -13,8 +14,11 @@ namespace VisualStudioEX3.Artemis
     public class PlayerNavigationController : MonoBehaviour
     {
         #region Constants
-        private const string INPUT_MAP_ASSET_NAME = "Default";
-        private const string NAVIGATION_INPUT_AXIS_NAME = "Navigation";
+        private const string INPUT_MAP_ASSET_NAME = "Navigation";
+        private const string MOUSE_NAVIGATION_INPUT_AXIS_NAME = "Mouse Navigation";
+        private const string KEYBOARD_NAVIGATION_INPUT_AXIS_NAME = "Keyboard Navigation";
+        private const string MOUSE_NAVIGATION_INPUT_ACTION_NAME = "Enable Mouse Navigation";
+        private const string KEYBOARD_NAVIGATION_INPUT_ACTION_NAME = "Enable Keyboard Navigation";
 
         private const float MIN_MOVEMENT_SPEED = 0.5f;
         private const float MAX_MOVEMENT_SPEED = 10f;
@@ -22,7 +26,10 @@ namespace VisualStudioEX3.Artemis
         #endregion
 
         #region Internal vars
-        InputAxis _navigationAxis;
+        InputAxis _mouseNavigationAxis;
+        InputAxis _keyboardNavigationAxis;
+        InputAction _mouseNavigationAction;
+        InputAction _keyboardNavigationAction;
         #endregion
 
         #region Inspector fields
@@ -33,36 +40,67 @@ namespace VisualStudioEX3.Artemis
         #region Initializers & Terminators
         private void Awake()
         {
-            this.SubscribeOnPlayerMovesEvent();
+            this.SubscribeOnNavigationEvent(eventListener: this.OnMouseNavigation,
+                inputAxis: out this._mouseNavigationAxis,
+                inputAction: out this._mouseNavigationAction,
+                inputMapAssetName: INPUT_MAP_ASSET_NAME,
+                inputAxisName: MOUSE_NAVIGATION_INPUT_AXIS_NAME,
+                inputActionName: MOUSE_NAVIGATION_INPUT_ACTION_NAME);
+
+            this.SubscribeOnNavigationEvent(eventListener: this.OnKeyboardNavigation,
+                inputAxis: out this._keyboardNavigationAxis,
+                inputAction: out this._keyboardNavigationAction,
+                inputMapAssetName: INPUT_MAP_ASSET_NAME,
+                inputAxisName: KEYBOARD_NAVIGATION_INPUT_AXIS_NAME,
+                inputActionName: KEYBOARD_NAVIGATION_INPUT_ACTION_NAME);
         }
 
         private void OnDestroy()
         {
-            this.UnsubscribeOnPlayerMovesEvent();
+            this.UnsubscribeOnNavigationEvent(eventListener: this.OnMouseNavigation, inputAxis: this._mouseNavigationAxis);
+            this.UnsubscribeOnNavigationEvent(eventListener: this.OnKeyboardNavigation, inputAxis: this._keyboardNavigationAxis);
         }
         #endregion
 
         #region Methods & Functions
-        private void SubscribeOnPlayerMovesEvent()
+        private void SubscribeOnNavigationEvent(Action<Vector2> eventListener, 
+            out InputAxis inputAxis,
+            out InputAction inputAction, 
+            string inputMapAssetName, 
+            string inputAxisName, 
+            string inputActionName)
         {
-            InputMapAsset inputMap = InputManager.Instance.GetInputMap(INPUT_MAP_ASSET_NAME);
-            this._navigationAxis = inputMap.GetAxis(NAVIGATION_INPUT_AXIS_NAME);
-            this._navigationAxis.OnAxisMove += this.OnPlayerMoves;
+            InputMapAsset inputMap = InputManager.Instance.GetInputMap(inputMapAssetName);
+
+            inputAxis = inputMap.GetAxis(inputAxisName);
+            inputAction = inputMap.GetAction(inputActionName);
+
+            inputAxis.OnAxisMove += eventListener;
         }
 
-        private void UnsubscribeOnPlayerMovesEvent() => this._navigationAxis.OnAxisMove -= this.OnPlayerMoves;
+        private void UnsubscribeOnNavigationEvent(Action<Vector2> eventListener, InputAxis inputAxis)
+        {
+            inputAxis.OnAxisMove -= eventListener;
+        }
 
         private void MovePlayer(Vector2 delta)
         {
             // TODO: Control the bounds of the scenary to avoid the player moves outside.
-            this.transform.position += this._movementSpeed * Time.deltaTime * (Vector3)delta;
+            this.transform.position += this._movementSpeed * Time.deltaTime * new Vector3(delta.x, 0f, delta.y);
         }
         #endregion
 
         #region Event listeners
-        private void OnPlayerMoves(Vector2 delta)
+        private void OnMouseNavigation(Vector2 delta)
+        {  
+            if (this._mouseNavigationAction.IsPressed)
+                this.MovePlayer(delta);
+        }
+
+        private void OnKeyboardNavigation(Vector2 delta)
         {
-            this.MovePlayer(delta);
+            if (this._keyboardNavigationAction.IsPressed)
+                this.MovePlayer(delta);
         }
         #endregion
     }
