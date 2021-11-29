be8523a581aec1a805a36fce5775acc828ae7ab7
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-11-29 11:55:40 +0100

    Implemented LayerAttribute and LayerDrawer to render layer fields

---
+++ Assets/Frameworks/Core.Contracts/Attributes/LayerAttribute.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace VisualStudioEX3.Artemis.Framework.Core.Contracts
+{
+    /// <summary>
+    /// Attribute used to make a int variable in a script be restricted to layer values.
+    /// </summary>
+    public class LayerAttribute : PropertyAttribute
+    {
+    }
+}
---
+++ Assets/Frameworks/Core.Editor.Components/PropertyDrawers/LayerDrawer.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using VisualStudioEX3.Artemis.Framework.Core.Contracts;
+
+namespace VisualStudioEX3.Artemis.Framework.Core.Editor.Components
+{
+    [CustomPropertyDrawer(typeof(LayerAttribute))]
+    public class LayerDrawer : PropertyDrawer
+    {
+        #region Methods & Functions
+        private bool CheckPropertyType(SerializedProperty property) => property.propertyType == SerializedPropertyType.Integer;
+
+        private void DrawControl(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+        }
+
+        private ArgumentException FormatArgumentException(string propertyName)
+        {
+            return new ArgumentException($"{nameof(LayerAttribute)}: The property \"{propertyName}\" must be an integer field.");
+        }
+        #endregion
+
+        #region Event listeners
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (this.CheckPropertyType(property))
+                this.DrawControl(position, property, label);
+            else
+                throw this.FormatArgumentException(property.name);
+        }
+        #endregion
+    }
+}
