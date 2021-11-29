25ac1532264b09a8bf81f51000799bf3c9b428c6
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-11-29 02:23:30 +0100

    Created base SceneManager component

---
+++ Assets/Frameworks/Core.Components/Components/SceneManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using VisualStudioEX3.Artemis.Framework.Core.Contracts.Models;
+
+namespace VisualStudioEX3.Artemis.Framework.Core.Components
+{
+    public class SceneManager : MonoBehaviour
+    {
+        #region Inspector fields
+        [SerializeField]
+        private SceneAsset[] _scenes; 
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
