10c549e3ccc4332dcd15c2bb8029f8d4c1d65aa8
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-11-29 10:44:56 +0100

    Created a TurretPlacement prefab
    - Implemented the TurretPlacementController component.
    - Added the BlinkMaterial component.
    - Added new layer: "Turret Placement". Used for cast the mouse cursor.

---
+++ Assets/Entities/TurretPlacement/Scripts/TurretPlacementTriggerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VisualStudioEX3.Artemis.Turret.Placement
+{
+    public class TurretPlacementTriggerController : MonoBehaviour
+    {
+        [SerializeField]
+        private Renderer _renderer;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
