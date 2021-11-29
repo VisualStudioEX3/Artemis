4d61b85bcefb0685dd874410adf91f418dfc8bbc
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-11-29 02:42:58 +0100

    Created ComponentMenuPaths constants for Core.Contracts library
    - Added paths for SceneManager component.

---
+++ Assets/Frameworks/Core.Contracts/Constants/ComponentMenuPaths.cs
@@ -0,0 +1,11 @@
+namespace VisualStudioEX3.Artemis.Framework.Core.Contracts.Constants
+{
+    public static class ComponentMenuPaths
+    {
+        #region Constants
+        private const string SCENE_MANAGER_BASE_COMPONENT_MENU_NAMESPACE = CoreConstants.BASE_MENU_NAMESPACE + "Scene Manager/";
+
+        public const string SCENE_MANAGER_COMPONENT_MENU_NAMESPACE = SCENE_MANAGER_BASE_COMPONENT_MENU_NAMESPACE + "Scene Manager (Component)"; 
+        #endregion
+    }
+}
