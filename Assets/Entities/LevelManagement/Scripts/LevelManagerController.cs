7f9c60502647415e0282394805adff7ff2732fd5
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-03 23:51:55 +0100

    Added public enemy spawners and player base references in the LevelManagerController singleton
    - Added PlayerBase property to get access to the PlayerBaseController instance.
    - Added an enumerable of EnemySpawnerControllers in scene.

--- Assets/Entities/LevelManagement/Scripts/LevelManagerController.cs
+++ Assets/Entities/LevelManagement/Scripts/LevelManagerController.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
+using VisualStudioEX3.Artemis.Assets.EnemySystem.Controllers;
 using VisualStudioEX3.Artemis.Assets.LevelManagement.Constants;
+using VisualStudioEX3.Artemis.Assets.Player.Controllers;
 using VisualStudioEX3.Artemis.Assets.Scenes.Controllers;
 
 namespace VisualStudioEX3.Artemis.Assets.LevelManagement
@@ -7,13 +10,23 @@ namespace VisualStudioEX3.Artemis.Assets.LevelManagement
     [AddComponentMenu(ComponentMenuPaths.LEVEL_MANAGER_CONTROLLER_COMPONENT_MENU_NAMESPACE), DisallowMultipleComponent]
     public class LevelManagerController : MonoBehaviour
     {
+        #region Properties
+        public PlayerBaseController PlayerBase { get; private set; }
+        public IEnumerable<EnemySpawnerController> EnemySpawnLocations { get; private set; }
+        #endregion
+
         #region Inspector fields
         [SerializeField]
         private Renderer _editorBattlegroundGrid;
         #endregion
 
         #region Initializers
-        private void Awake() => this.InitializeLevel();
+        private void Awake()
+        {
+            this.InitializeLevel();
+            this.GetPlayerBaseObject();
+            this.GetAllEnemySpawnersInScene();
+        }
         #endregion
 
         #region Methods & Functions
@@ -24,6 +37,10 @@ namespace VisualStudioEX3.Artemis.Assets.LevelManagement
 
             this._editorBattlegroundGrid.enabled = false;
         }
+
+        private void GetPlayerBaseObject() => this.PlayerBase = GameObject.FindObjectOfType<PlayerBaseController>();
+
+        private void GetAllEnemySpawnersInScene() => this.EnemySpawnLocations = GameObject.FindObjectsOfType<EnemySpawnerController>();
         #endregion
     }
 }
