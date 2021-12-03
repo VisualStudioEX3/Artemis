484086fc8e295be8c9794773b3d3156614b120a5
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-03 17:42:59 +0100

    Implemented an empty sealed WallTileController script for the WallTile prefab
    - Required for the code that destroy all instances from editor.
    - Updated WallTile prefab.

---
+++ Assets/Entities/LevelGenerator/Scripts/Controller/WallTileController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using VisualStudioEX3.Artemis.Assets.LevelGenerator.Controllers;
+
+namespace VisualStudioEX3.Artemis.Assets.LevelGenerator.Controller
+{
+    /// <summary>
+    /// Wall Tile Controller.
+    /// </summary>
+    /// <remarks>Used only to identify this game objects when are destroyed from <see cref="LevelGeneratorController"/>.</remarks>
+    public sealed class WallTileController : MonoBehaviour
+    {
+    }
+}
