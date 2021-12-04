59933a4875e0fba51c69ea1044826dd72efc3257
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-04 23:16:45 +0100

    Updated TurretPlacementController to allow quick instantiate test turrets

--- Assets/Entities/TurretSystem/Scripts/TurretPlacementController.cs
+++ Assets/Entities/TurretSystem/Scripts/TurretPlacementController.cs
@@ -7,13 +7,18 @@ namespace VisualStudioEX3.Artemis.Assets.TurretSystem.Controllers
         #region Inspector fields
         [SerializeField]
         private Renderer _triggerRenderer;
+        [SerializeField]
+        private TurretController _testTurret;
         #endregion
 
         #region Methods & Functions
         public void CreateTurret()
         {
             // TODO: Implements instanciating of the turret.
-            this.DisableTrigger();
+            // Testing Turret 1:
+            GameObject.Instantiate(this._testTurret, this.transform);
+
+            this.DisableTrigger(); // TODO: Replaces disable trigger by disabling the gameObject itself.
         }
 
         private void DisableTrigger() => this._triggerRenderer.enabled = false;
