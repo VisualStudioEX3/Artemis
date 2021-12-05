428225cf9aa3c0ab7ebe3ca5c47b5f2e8cd4981c
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-05 21:58:16 +0100

    Fixed physics on BulletTrigger component
    - Updated prefab.

--- Assets/Entities/Bullets/Scripts/BulletController.cs
+++ Assets/Entities/Bullets/Scripts/BulletController.cs
@@ -9,23 +9,23 @@ namespace VisualStudioEX3.Artemis.Assets.Bullets.Controllers
     {
         #region Constants
         private const float MIN_SPEED = 0.5f;
-        private const float MAX_SPEED = 10f;
+        private const float MAX_SPEED = 100f;
         private const float DEFAULT_SPEED = 1f;
 
         private const float MIN_LIFE_TIME = 1f;
         private const float MAX_LIFE_TIME = 10f;
-        private const float DEFAULT_LIFE_TIME = 1f; 
+        private const float DEFAULT_LIFE_TIME = 1f;
         #endregion
 
         #region Internal vars
         private AttackController _attackController;
-        private Rigidbody _rigidBody; 
+        private Rigidbody _rigidBody;
         #endregion
 
         #region Inspector fields
         [SerializeField, Range(MIN_SPEED, MAX_SPEED)]
         private float _speed = DEFAULT_SPEED;
-        
+
         [SerializeField, Range(MIN_LIFE_TIME, MAX_LIFE_TIME)]
         private float _lifeTime = DEFAULT_LIFE_TIME;
         #endregion
@@ -44,28 +44,30 @@ namespace VisualStudioEX3.Artemis.Assets.Bullets.Controllers
         }
         #endregion
 
+        #region Update logic
+        private void FixedUpdate() => this.MoveBullet();
+        #endregion
+
         #region Methods & Functions
-        private void SetupAttackController()
-        {
-            this._attackController = this.GetComponent<AttackController>();
-            this._attackController.OnImpact += this.OnImpact;
-        }
+        private void SetupAttackController() => this._attackController = this.GetComponent<AttackController>();
 
         private void SetupRigidBody()
         {
             this._rigidBody = this.GetComponent<Rigidbody>();
-            this.SetupForceToRigidBody();
+            this._rigidBody.freezeRotation = true;
         }
 
         private Vector3 CalculateVectorImpulse() => this.transform.forward * this._speed;
 
-        private void SetupForceToRigidBody() => this._rigidBody.AddForce(force: this.CalculateVectorImpulse(), mode: ForceMode.Impulse);
+        private void MoveBullet() => this._rigidBody.velocity = this.CalculateVectorImpulse();
 
         private void DisableBullet() => this.gameObject.SetActive(false);
         #endregion
 
         #region Event listeners
-        private void OnImpact() => this.DisableBullet();
+        private void OnCollisionEnter(Collision collision) => this.DisableBullet();
+
+        private void OnTriggerEnter(Collider other) => this.DisableBullet();
         #endregion
     }
 }
