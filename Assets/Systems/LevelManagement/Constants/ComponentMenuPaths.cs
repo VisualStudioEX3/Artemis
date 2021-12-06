a07644ff87d03dc2e4b56515926ac55b23321e0d
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-06 20:57:28 +0100

    Fixed component name for SceneManager component menu namespace

--- Assets/Systems/LevelManagement/Constants/ComponentMenuPaths.cs
+++ Assets/Systems/LevelManagement/Constants/ComponentMenuPaths.cs
@@ -6,7 +6,7 @@ namespace VisualStudioEX3.Artemis.Assets.LevelManagement.Constants
     {
         #region Constants
         private const string LEVEL_MANAGEMENT_BASE_COMPONENT_MENU_NAMESPACE = CoreConstants.BASE_MENU_NAMESPACE + "Level Management/";
-        public const string LEVEL_MANAGER_CONTROLLER_COMPONENT_MENU_NAMESPACE = LEVEL_MANAGEMENT_BASE_COMPONENT_MENU_NAMESPACE + "Level Manager Controller (Singleton manager)";
+        public const string LEVEL_MANAGER_CONTROLLER_COMPONENT_MENU_NAMESPACE = LEVEL_MANAGEMENT_BASE_COMPONENT_MENU_NAMESPACE + "Scene Manager Controller (Singleton manager)";
         #endregion
     }
 }
