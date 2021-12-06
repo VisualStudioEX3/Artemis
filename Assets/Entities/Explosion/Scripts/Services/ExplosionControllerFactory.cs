4b04a3ca1a72f5b0ffb3df8eea1faeadbff7291b
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-06 07:54:32 +0100

    Implemented Explosion prefab
    - Implemented too a factory singleton to get instances.

---
+++ Assets/Entities/Explosion/Scripts/Services/ExplosionControllerFactory.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using VisualStudioEX3.Artemis.Assets.Explosion.Controllers;
+using VisualStudioEX3.Artemis.Framework.Core.Components;
+using VisualStudioEX3.Artemis.Framework.Core.Contracts.Interfaces;
+using VisualStudioEX3.Artemis.Framework.Core.ServiceProvider;
+
+namespace VisualStudioEX3.Artemis.Assets.Entities.Explosion.Scripts.Services
+{
+    public class ExplosionControllerFactory : MonoBehaviourSingleton<ExplosionControllerFactory>
+    {
+        #region Internal vars
+        private IUnityObjectPoolService<ExplosionController> _explosionPool;
+        #endregion
+
+        #region Inspector fields
+        [SerializeField]
+        private ExplosionController _prefab;
+        [SerializeField]
+        private int _maxExplosions = 20;
+        #endregion
+
+        #region Initializers & Terminators
+        public override void Awake()
+        {
+            base.Awake();
+            this.CreateExplosionPool();
+        }
+
+        public override void OnDestroy()
+        {
+            this._explosionPool.Dispose();
+            base.OnDestroy();
+        }
+        #endregion
+
+        #region Methods & Functions
+        private void CreateExplosionPool()
+        {
+            this._explosionPool = (IUnityObjectPoolService<ExplosionController>)CoreServiceFactory.Factory.GetService(typeof(IUnityObjectPoolService<>), typeof(ExplosionController));
+            this._explosionPool.Initialize(
+                prefab: this._prefab,
+                capacity: this._maxExplosions,
+                availableInstancePredicate: (explosion) => !explosion.gameObject.activeInHierarchy,
+                releaseInstancePredicate: (explosion) => explosion.gameObject.SetActive(false));
+            this._explosionPool.ReleaseAllInstances();
+        }
+
+        public bool TryGetInstance(out ExplosionController instance)
+        {
+            bool ret = this._explosionPool.TryGetNext(out instance);
+
+            if (!ret)
+            {
+                print("No explosion available.");
+            }
+
+            return ret;
+        }
+        #endregion
+    }
+}
