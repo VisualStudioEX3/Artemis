56fcb353c1f770b7bced71e25913830be88882a4
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-06 07:53:22 +0100

    Implemented ExplosiveBulletController component
    - Applied in Bullet Type 3 prefab, to generate an explosion on collision.

---
+++ Assets/Entities/Bullets/Scripts/ExplosiveBulletController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using VisualStudioEX3.Artemis.Assets.Entities.Explosion.Scripts.Services;
+using VisualStudioEX3.Artemis.Assets.Explosion.Controllers;
+
+namespace VisualStudioEX3.Artemis.Assets.Bullets.Controllers
+{
+    public class ExplosiveBulletController : BulletController
+    {
+        #region Event listeners
+        public override void OnTriggerEnter(Collider other)
+        {
+            this.InstanceExplosion();
+            base.OnTriggerEnter(other);
+        }
+
+        public override void OnCollisionEnter(Collision collision)
+        {
+            this.InstanceExplosion();
+            base.OnCollisionEnter(collision);
+        }
+        #endregion
+
+        #region Methods & Functions
+        private void InstanceExplosion()
+        {
+            if (ExplosionControllerFactory.Instance.TryGetInstance(out ExplosionController instance))
+            {
+                instance.transform.position = this.transform.position;
+                instance.transform.rotation = Quaternion.identity;
+                instance.gameObject.SetActive(true);
+            }
+        } 
+        #endregion
+    }
+}
