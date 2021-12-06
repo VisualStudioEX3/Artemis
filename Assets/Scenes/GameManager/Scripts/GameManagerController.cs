dcaf1a7f600269a4b333a5686bf7106defc6a181
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-06 20:33:38 +0100

    Created a static event to notifies when the GameManager is initialized
    - Avoid to use the MonoBehaviourSingleton because the initialization process and because we don't need the instance globaly accessible.

--- Assets/Scenes/GameManager/Scripts/GameManagerController.cs
+++ Assets/Scenes/GameManager/Scripts/GameManagerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using VisualStudioEX3.Artemis.Assets.EconomySystem.Controllers;
@@ -7,14 +8,28 @@ using VisualStudioEX3.Artemis.Framework.InputManager.Components;
 
 namespace VisualStudioEX3.Artemis.Assets.Scenes.Controllers
 {
+    /// <summary>
+    /// Main manager of the game.
+    /// </summary>
+    /// <remarks>All starts from here.</remarks>
+    [DisallowMultipleComponent]
     public class GameManagerController : MonoBehaviour
     {
+        #region Events
+        /// <summary>
+        /// Notifies when the all managers are initialized.
+        /// </summary>
+        public static event Action OnGameManagerIsIntialized;
+        #endregion
+
         #region Initializer
         private IEnumerator Start()
         {
             yield return this.WaitForManagerInitialization<InputManager>();
             yield return this.WaitForManagerInitialization<SceneManager>();
             yield return this.WaitForManagerInitialization<EconomyManager>();
+
+            GameManagerController.OnGameManagerIsIntialized?.Invoke();
 
             SceneManager.Instance.LoadStartScreen();
         }
