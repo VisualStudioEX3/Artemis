a9792fb75441e333eab118f9b0876ecd4386b950
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-07 05:41:32 +0100

    Implemented initial amount of credits at start in the EconomyManager

--- Assets/Systems/EconomySystem/Scripts/Controllers/EconomyManager.cs
+++ Assets/Systems/EconomySystem/Scripts/Controllers/EconomyManager.cs
@@ -8,34 +8,40 @@ namespace VisualStudioEX3.Artemis.Assets.EconomySystem.Controllers
     [DisallowMultipleComponent]
     public class EconomyManager : MonoBehaviourSingleton<EconomyManager>
     {
+        #region Constants
+        private const int DEFAULT_INITIAL_CREDITS = 15; 
+        #endregion
+
         #region Inspector fields
         [SerializeField, Tooltip("Max amount of units that the player can earn."), Min(0)]
-        private int _maxUnits;
+        private int _maxCredits;
+        [SerializeField, Tooltip("Is recomended to setup an initial value to allow the player put a few turrets at the start."), Min(0)]
+        private int _initialCurrentCredits = DEFAULT_INITIAL_CREDITS;
         #endregion
 
         #region Properties
         /// <summary>
-        /// Current amount of units.
+        /// Current amount of credits.
         /// </summary>
-        public int CurrentUnits { get; private set; }
+        public int CurrentCredits { get; private set; }
 
         /// <summary>
-        /// Max amount of units that the player can earn.
+        /// Max amount of credits that the player can earn.
         /// </summary>
-        public int MaxUnits => this._maxUnits;
+        public int MaxCredits => this._maxCredits;
         #endregion
 
         #region Events
         /// <summary>
-        /// Notifies when the player earn units.
+        /// Notifies when the player earn credits.
         /// </summary>
-        /// <remarks>Returns the amount of units earned and the current total units.</remarks>
+        /// <remarks>Returns the amount of credits earned and the current total credits.</remarks>
         public event Action<int, int> OnEarn;
 
         /// <summary>
         /// Notifies when the player achieve a sucessfull payment operation.
         /// </summary>
-        /// <remarks>Returns the amount of units used to pay and the rest of the units left.</remarks>
+        /// <remarks>Returns the amount of credits used to pay and the rest of the credits left.</remarks>
         public event Action<int, int> OnSucessfullPayment;
 
         /// <summary>
@@ -44,26 +50,38 @@ namespace VisualStudioEX3.Artemis.Assets.EconomySystem.Controllers
         public event Action OnReset;
         #endregion
 
+        #region Initializers & Terminators
+        public override void Awake()
+        {
+            base.Awake();
+            this.CurrentCredits = this._initialCurrentCredits;
+        }
+
+        public override void OnDestroy() => base.OnDestroy();
+        #endregion
+
         #region Methods & Functions
+        public override bool IsPersistentBetweenScenes() => true;
+
         private void EarnUnits(int units)
         {
-            this.CurrentUnits = Mathf.Min(this.CurrentUnits + units, this._maxUnits);
-            this.OnEarn?.Invoke(units, this.CurrentUnits);
+            this.CurrentCredits = Mathf.Min(this.CurrentCredits + units, this._maxCredits);
+            this.OnEarn?.Invoke(units, this.CurrentCredits);
         }
 
         /// <summary>
-        /// try to pay with an amount of units.
+        /// try to pay with an amount of credits.
         /// </summary>
         /// <param name="price">Units to pay.</param>
-        /// <returns>Returns <see langword="true"/> if the player have enough units and rest them from the current units.</returns>
+        /// <returns>Returns <see langword="true"/> if the player have enough credits and rest them from the current credits.</returns>
         public bool TryPayUnits(int price)
         {
-            int afterPayUnits = this.CurrentUnits - price;
+            int afterPayCredits = this.CurrentCredits - price;
 
-            if (afterPayUnits >= 0)
+            if (afterPayCredits >= 0)
             {
-                this.CurrentUnits = afterPayUnits;
-                this.OnSucessfullPayment?.Invoke(price, this.CurrentUnits);
+                this.CurrentCredits = afterPayCredits;
+                this.OnSucessfullPayment?.Invoke(price, this.CurrentCredits);
 
                 return true;
             }
@@ -72,11 +90,11 @@ namespace VisualStudioEX3.Artemis.Assets.EconomySystem.Controllers
         }
 
         /// <summary>
-        /// Reset to zero the current amount of units.
+        /// Reset to initial value the current amount of credits.
         /// </summary>
         public void ResetUnits()
         {
-            this.CurrentUnits = 0;
+            this.CurrentCredits = this._initialCurrentCredits;
             this.OnReset?.Invoke();
         }
         #endregion
@@ -85,7 +103,7 @@ namespace VisualStudioEX3.Artemis.Assets.EconomySystem.Controllers
         /// <summary>
         /// Gets a reward when a enemy dead.
         /// </summary>
-        /// <param name="reward">Amount of units to earn.</param>
+        /// <param name="reward">Amount of credits to earn.</param>
         /// <remarks>All <see cref="EnemyController"/> instances must be subscribe this event listener to his <see cref="EnemyController.OnDead"/> event.</remarks>
         public void OnEnemyDead(int reward) => this.EarnUnits(reward);
         #endregion
