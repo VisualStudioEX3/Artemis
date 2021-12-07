44b1d79421cf70afdb303e5427e8beb3a9776d79
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-07 07:10:49 +0100

    Implemented Game Over screen logic in PlayerBaseController
    - Updated prefab.

--- Assets/Systems/Player/Scripts/PlayerBaseController.cs
+++ Assets/Systems/Player/Scripts/PlayerBaseController.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using UnityEngine;
 using VisualStudioEX3.Artemis.Assets.Common.Controllers;
 using VisualStudioEX3.Artemis.Assets.Explosion.Controllers;
+using VisualStudioEX3.Artemis.Assets.GameOver.Controllers;
 
 namespace VisualStudioEX3.Artemis.Assets.Player.Controllers
 {
@@ -10,13 +12,21 @@ namespace VisualStudioEX3.Artemis.Assets.Player.Controllers
     [DisallowMultipleComponent, RequireComponent(typeof(HealthController), typeof(Rigidbody), typeof(SphereCollider))]
     public class PlayerBaseController : MonoBehaviour
     {
+        #region Constants
+        private const float DEFAULT_WAIT_UNTIL_SHOW_GAME_OVER_SCREEN = 3f; 
+        #endregion
+
         #region Internal vars
         private HealthController _healthController;
         #endregion
 
         #region Inspector fields
         [SerializeField]
+        private GameObject _root;
+        [SerializeField]
         private ExplosionController _explosion;
+        [SerializeField, Min(0)]
+        private float _waitUntilShowGameOverScreen = DEFAULT_WAIT_UNTIL_SHOW_GAME_OVER_SCREEN;
         #endregion
 
         #region Initializers
@@ -34,13 +44,15 @@ namespace VisualStudioEX3.Artemis.Assets.Player.Controllers
             this.SubscribeHealthEvents();
         }
 
-        private void DisableObject() => this.gameObject.SetActive(false);
+        private void DisableObject() => this._root.SetActive(false);
 
         private void Explode()
         {
             this._explosion.transform.parent = null;
             this._explosion.gameObject.SetActive(true);
         }
+
+        private void SetGameOverScreen() => this.StartCoroutine(this.GameOverCoroutine());
         #endregion
 
         #region Event listeners
@@ -48,8 +60,16 @@ namespace VisualStudioEX3.Artemis.Assets.Player.Controllers
         {
             this.Explode();
             this.DisableObject();
-            // TODO: Shows a Game Over screen.
+            this.SetGameOverScreen();
         }
+        #endregion
+
+        #region Coroutines
+        private IEnumerator GameOverCoroutine()
+        {
+            yield return new WaitForSeconds(this._waitUntilShowGameOverScreen);
+            GameOverController.Instance.Show();
+        } 
         #endregion
     }
 }
