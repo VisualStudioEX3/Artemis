25b74588adfc9215786b5ce8c9032e9ad997b408
José Miguel Sánchez Fernández <jose.miguel@visualstudioex3.com>
2021-12-07 06:16:44 +0100

    Completed PlayerBaseController logic

--- Assets/Systems/Player/Scripts/PlayerBaseController.cs
+++ Assets/Systems/Player/Scripts/PlayerBaseController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using VisualStudioEX3.Artemis.Assets.Common.Controllers;
+using VisualStudioEX3.Artemis.Assets.Explosion.Controllers;
 
 namespace VisualStudioEX3.Artemis.Assets.Player.Controllers
 {
@@ -9,5 +10,46 @@ namespace VisualStudioEX3.Artemis.Assets.Player.Controllers
     [DisallowMultipleComponent, RequireComponent(typeof(HealthController), typeof(Rigidbody), typeof(SphereCollider))]
     public class PlayerBaseController : MonoBehaviour
     {
+        #region Internal vars
+        private HealthController _healthController;
+        #endregion
+
+        #region Inspector fields
+        [SerializeField]
+        private ExplosionController _explosion;
+        #endregion
+
+        #region Initializers
+        private void Awake() => this.SetupHealthComponent();
+        #endregion
+
+        #region Methods & Functions
+        private void ResolveHealthComponent() => this._healthController = this.GetComponent<HealthController>();
+
+        private void SubscribeHealthEvents() => this._healthController.OnDeath += this.OnDead;
+
+        private void SetupHealthComponent()
+        {
+            this.ResolveHealthComponent();
+            this.SubscribeHealthEvents();
+        }
+
+        private void DisableObject() => this.gameObject.SetActive(false);
+
+        private void Explode()
+        {
+            this._explosion.transform.parent = null;
+            this._explosion.gameObject.SetActive(true);
+        }
+        #endregion
+
+        #region Event listeners
+        private void OnDead()
+        {
+            this.Explode();
+            this.DisableObject();
+            // TODO: Shows a Game Over screen.
+        }
+        #endregion
     }
 }
